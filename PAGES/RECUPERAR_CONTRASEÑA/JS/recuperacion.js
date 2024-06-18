ce802b4e1885a8599b13c31e1d51d74872ccdaf8
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2024-06-17 23:17:29 -0300

    Resubida modificaciones recuperacion
    Aparentemente no se subieron los archivos de la pagina de Recuperar contraseña.

---
+++ PAGES/RECUPERAR_CONTRASEÑA/JS/recuperacion.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const avanze = document.querySelectorAll('.siguiente');
+    const volver = document.querySelectorAll('.volver');
+    const roots = document.querySelectorAll('.root');
+
+    roots[0].classList.add('active');
+
+    avanze.forEach((sig_btn, index) => {
+        sig_btn.addEventListener('click', (e) => {
+            e.preventDefault(); // Prevenir el comportamiento por defecto del botón
+            if (index !== 2) {
+                // Remover la clase active del root actual
+                roots[index].classList.remove('active');
+
+                // Calcular el índice del próximo root
+                const nextIndex = index + 1;
+
+                // Añadir la clase active al próximo root si existe
+                if (nextIndex < roots.length) {
+                    roots[nextIndex].classList.add('active');
+                }
+            }
+        });
+    });
+
+    volver.forEach((volver_btn, index) => {
+        volver_btn.addEventListener('click', (e) => {
+            e.preventDefault(); // Prevenir el comportamiento por defecto del botón
+            // Verificar si no estamos en el primer elemento .root
+            if (index !== 0) {
+                // Remover la clase active del root actual
+                roots[index].classList.remove('active');
+
+                // Calcular el índice del root anterior
+                const prevIndex = index - 1;
+
+                // Añadir la clase active al root anterior si existe
+                if (prevIndex >= 0) {
+                    roots[prevIndex].classList.add('active');
+                }
+            }
+        });
+    });
+});
