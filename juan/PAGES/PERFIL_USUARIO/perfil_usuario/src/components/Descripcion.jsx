3ac98a14ee394c6ca67c5739747821943563c2bf
agustin-oliverio <agustin.oliverioet32@gmail.com>
2024-08-13 18:50:49 -0300

    Añadida de modales
    Ahora la pagina tiene modales y es educada. Levanta el dedo meñique cuando toma té

--- juan/PAGES/PERFIL_USUARIO/perfil_usuario/src/components/Descripcion.jsx
+++ juan/PAGES/PERFIL_USUARIO/perfil_usuario/src/components/Descripcion.jsx
@@ -32,6 +32,25 @@ export const Descripcion = () => {
                     />
                 </div>
             </Box>
+
+
+
+            <Button onClick={handleOpen}><Fab color="secondary" aria-label="edit"><EditIcon /></Fab></Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
         </div>
     );
 }
