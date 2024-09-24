94193ce5dc021e3e11aad2d81e5c7da88e9b1039
DamianMaldonado <126907765+DamianMaldonado@users.noreply.github.com>
2024-09-23 23:22:51 -0300

    Update código_database.sql
    Se eliminan 4 tablas, education, chat, contact y users_education

--- código_database.sql
+++ código_database.sql
@@ -20,32 +20,6 @@ create table users(
     primary key (userId)
 );
 
-create table contact(
-    contactId varchar(70) not null,
-    user1Id varchar(70) not null,
-    user2Id varchar(70) not null,
-    primary key(contactId),
-    foreign key (user1Id) references users (userId),
-    foreign key (user2Id) references users (userId)
-);
-
-create table education(
-    name varchar(70) not null,
-    description text not null,
-    speciality text not null,
-    primary key (name)
-);
-
-create table chat(
-    chatId int(30) not null,
-    messageDate date not null,
-    content text not null,
-    emisor varchar(70) not null,
-    receptor varchar(70) not null,
-    primary key(chatId),
-    FOREIGN KEY (emisor) references users (userId),
-    FOREIGN KEY (receptor) references users (userId)
-);
 
 create table posts(
     postId int(30) not null,
@@ -88,15 +62,7 @@ create table jobsHistory(
 );
 
 
-create table users_education(
-    id varchar(70) not null,
-    userId varchar(70) not null,
-    educationName varchar(70) not null,
-    description text not null,
-    primary key (id),
-    foreign key (userId) references users (userId),
-    foreign key (educationName) references education (name)
-);
+
 
 insert into users(userId, username, role, userPassword, createdAt, age, phoneNumber, mail, subscription, address)
 values("1", "martuski", "administrador", "pepe", '2024-4-23', 17, 1170809090,"martin.galeanoet32@gmail.com",true ,"aa"),
